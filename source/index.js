21f6c4de978b2ec50b94ee77593b83c782828737
Christian Auth <c.auth@syzygy.de>
2018-01-15 13:31:20 +0100

    feat: adds simplified configuration

--- source/index.js
+++ source/index.js
@@ -1,8 +1,32 @@
+'use strict';
+
 /**
- *
+ * Registers with default configurations
+ */
+function register(configuration, options)
+{
+    // Commands
+    configuration.commands.add(require('./index.js').command.PhpExportCommand);
+
+    // Export
+    configuration.mappings.add(require('./index.js').export.PhpRenderer,
+        {
+            '!nodeRenderers': require('./index.js').export.renderer.rendererList
+        });
+    configuration.mappings.add(require('./index.js').export.PhpTransformer,
+        {
+            '!nodeTransformers': require('./index.js').export.transformer.transformerList
+        });    
+}
+
+
+/**
+ * Exports
+ * @ignore
  */
 module.exports =
 {
+    register: register,
     command: require('./command/index.js'),
     configuration: require('./configuration/index.js'),
     export: require('./export/index.js'),
